434d61fcd9c2126cb3ffcbfdf87e437b0a761406
shiv-chan <kshibuya31@gmail.com>
2021-06-25 13:36:35 -0700

    Add fetch API feature.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import { Container, Button, Row, Col } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
 
 export default function Hero() {
 	return (
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Hero from './Hero';
@@ -6,6 +6,30 @@ import HotelCard from './HotelCard';
 import { Row, Col } from 'react-bootstrap';
 
 export default function Home() {
+	const [data, setData] = useState(null);
+
+	// fetch fake-hotel api
+	const fetchHotel = async () => {
+		return (
+			await fetch('http://fake-hotel-api.herokuapp.com/api/hotels')
+		).json();
+	};
+
+	// set data
+	const setAllData = () => {
+		try {
+			fetchHotel().then((result) => setData(result));
+		} catch (e) {
+			console.error(`Failed to fetch Hotel API: ${e}`);
+		}
+	};
+
+	useEffect(() => {
+		setAllData();
+	}, []);
+
+	console.log(data);
+
 	return (
 		<>
 			<Header />
